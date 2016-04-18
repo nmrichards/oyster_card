e8a9999cb1e6d7cef3355fe57605ae1f8cc20787
Nick <nm.richards@yahoo.com>
2016-04-18 12:31:07 -0400

    Challenge 5 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  it "initializes with a balance of 0" do
+    expect(oystercard.balance).to eq 0
+  end
+
+  it "allows user to top up" do
+    random = rand(100)
+    expect { oystercard.top_up random }.to change { oystercard.balance }.by random
+  end
 
 end
